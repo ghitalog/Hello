69af7b03ccf6db3f3a6c4de0e52f1456eeba5e43
Gheorghe Slicari <ghita.slicari@mail.ru>
2022-12-26 20:33:32 +0000

    Created New function

--- Fodbal.cs
+++ Fodbal.cs
@@ -1,4 +1,17 @@
 ﻿Console.WriteLine("regarding");
 Console.WriteLine("Hello Victor");
 Console.WriteLine("world cup 2022 Argentina become");
-Console.WriteLine("The famous player was Messi"); 
+Console.WriteLine("The famous player was Messi");
+Console.Write("Tell me your name: ");
+string letter = Console.ReadLine();
+
+bool ifIsTrue = true;
+while (ifIsTrue)
+{
+    if (letter == "Victor")
+        Console.WriteLine("You are right");
+    letter = Console.ReadLine();
+}
+
+
+    Console.WriteLine("Try later");
